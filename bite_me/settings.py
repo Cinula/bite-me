a590b2270e0906bbd1aeddc6eb2d9095dc275378
Marcin Olszewski <cinula@gmail.com>
2025-04-10 22:15:57 +0000

    Enable WhiteNoise for static file compression on Heroku

--- bite_me/settings.py
+++ bite_me/settings.py
@@ -107,6 +107,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'booking', 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
